88b6fdb33fdafeddb06ac7c941c6a0ce37fb82e8
Mahmoud Saada <mahmoudsaada@gmail.com>
2013-02-10 17:36:35 -0700

    Extend BaseRemoteServiceAsync in the AuthServiceAsync

--- VLAB/src/edu/asu/snac/client/remote/auth/AuthServiceAsync.java
+++ VLAB/src/edu/asu/snac/client/remote/auth/AuthServiceAsync.java
@@ -2,10 +2,11 @@ package edu.asu.snac.client.remote.auth;
 
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
+import edu.asu.snac.client.remote.BaseRemoteServiceAsync;
 import edu.asu.snac.shared.web.AuthRequest;
 import edu.asu.snac.shared.web.WebResponse;
 
-public interface AuthServiceAsync {
+public interface AuthServiceAsync extends BaseRemoteServiceAsync<AuthRequest,String>{
 	public void onHandleRequest(AuthRequest request,
 			AsyncCallback<WebResponse<String>> callback)
 			throws IllegalArgumentException;
